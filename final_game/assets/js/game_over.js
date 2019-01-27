133c917cf07324d11e166f728279f518b57f9ee2
kemotk97 <44535006+kemotk97@users.noreply.github.com>
2019-01-27 18:26:06 +0100

    Update game_over.js

--- final_game/assets/js/game_over.js
+++ final_game/assets/js/game_over.js
@@ -11,8 +11,8 @@ var Game_Over = {
         this.add.button(0, 0, 'gameover', this.startGame, this);
 
         // Wynik
-        game.add.text(235, 450, "Wynik: ", { font: "bold 16px sans-serif", fill: "#ffffff", align: "center"});
-        game.add.text(350, 452, score.toString(), { font: "bold 20px sans-serif", fill: "#397200", align: "center" });
+        game.add.text(235, 400, "Wynik: ", { font: "bold 16px sans-serif", fill: "#ffffff", align: "center"});
+        game.add.text(350, 402, score.toString(), { font: "bold 20px sans-serif", fill: "#397200", align: "center" });
 
     },
 
